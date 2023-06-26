24725d4244837466caa68e3cabcd40c7f2583d11
Anjali <125981926+Anjali7876@users.noreply.github.com>
2023-06-26 13:13:30 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,43 +1,125 @@
-import React from 'react'
-import './Contact.css'
-// import TextField from '@mui/material/Button';
 
+import React, { useState } from 'react';
+import './Contact.css';
 import { FaHouseUser, FaTabletAlt, FaRegEnvelope } from "react-icons/fa";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [formError, setFormError] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form validation before submitting
+    if (!formData.name || !formData.email || !formData.message) {
+      setFormError('Please fill in all fields.');
+      return;
+    }
+
+    // Submit the form data to an API endpoint
+    fetch('https://644a0616a8370fb321430768.mockapi.io/student', {
+      method: 'POST',
+      body: JSON.stringify(formData)
+    })
+      .then(response => {
+        if (response.ok) {
+          // Form submission successful
+          alert('Form submitted successfully');
+          // Reset the form data
+          setFormData({ name: '', email: '', message: '' });
+          setFormError('');
+        } else {
+          // Form submission failed
+          throw new Error('Form submission failed.');
+        }
+      })
+      .catch(error => {
+        // Handle error during form submission
+        console.error('Error:', error);
+        setFormError('An error occurred. Please try again later.');
+      });
+  };
+
   return (
-    <div className="bgcontact" id="Contact">
-      
+    <div className="bgcontact">
       <div className="container">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-lg-8">
               <label className="contactlabel">Get in Touch</label><br /><br />
-              <textarea name="" id="" cols="100" rows="7" placeholder="Enter Message" className='form-control'>
-              </textarea><br />
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                cols="100"
+                rows="7"
+                placeholder="Enter Message"
+                className='form-control'
+              ></textarea><br />
               <div className="row">
-                <div className="col-lg-6">
-                {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-                {/* <TextField  label="Enter your name" variant="outlined" className='form-control' placeholder="Enter your name" required /> */}
-                  <input type="text" placeholder='Enter your name' className='form-control' required />
+                <div className="col-lg-6 pt-1">
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder='Enter your name'
+                    className='form-control'
+                    required
+                  />
                 </div>
-                <div className="col-lg-6">
-                  <input type="text" placeholder='Enter email address' className='form-control' required />
+                <div className="col-lg-6 pt-1">
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder='Enter email address'
+                    className='form-control'
+                    required
+                  />
                 </div>
               </div>
-              <button className=" Submitbtn">Submit</button>
+              {formError && <p className="error">{formError}</p>}
+              <button type="submit" className="Submitbtn">Submit</button>
             </div>
+
             <div className="col-lg-4 rightcolform">
-              <div className="rightcolindent" ><FaHouseUser className="contacticon" /><span className="iconaddress">
-                <a href="https://www.bing.com/maps/embed-a-map?src=SHELL&cp=31.708328902310328~76.93229414223765&lvl=11.07&form=LMLTEW" className="iconaddress">HP Distt. Mandi</a>
-              </span></div><br />
-              <div className="rightcolindent"><FaTabletAlt className="contacticon" /><span className="iconaddress"> +91 7876030787</span></div><br />
-              <div className="rightcolindent"><FaRegEnvelope className="contacticon" /><a href="mailto:attalanjali2001@gmail.com" className="iconaddress"> attalanjali2001@gmail.com</a></div>
+              <div className="rightcolindent">
+                <FaHouseUser className="contacticon" />
+                <span className="iconaddress">
+                  <a href="https://www.bing.com/maps/embed-a-map?src=SHELL&cp=31.708328902310328~76.93229414223765&lvl=11.07&form=LMLTEW" className="iconaddress">
+                    HP Distt. Mandi
+                  </a>
+                </span>
+              </div><br />
+              <div className="rightcolindent">
+                <FaTabletAlt className="contacticon" />
+                <span className="iconaddress"> +91 7876030787</span>
+              </div><br />
+              <div className="rightcolindent">
+                <FaRegEnvelope className="contacticon" />
+                <a href="mailto:attalanjali2001@gmail.com" className="iconaddress">
+                  attalanjali2001@gmail.com
+                </a>
+              </div>
             </div>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
+
+
